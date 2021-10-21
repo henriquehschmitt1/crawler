baefbf49f9bce3a49a36e1f68ea844646c7a7cec
Henrique Hermes Schmitt <38994134+henriquehschmitt1@users.noreply.github.com>
2021-10-21 10:40:32 -0300

    refact: refactoring code

--- index.js
+++ index.js
@@ -7,53 +7,58 @@ async function getAds(url, total) {
   const $ = cheerio.load(data);
 
   const listItems = $(".ui-search-results--without-disclaimer ol li");
-  const nextPage = $(".ui-search-pagination ul li")
+  const nextPage = $(".ui-search-pagination ul li");
 
-  const nextPageLink = $(nextPage).find(".andes-pagination__button--next > a").attr("href")
-  let ads = []
-  let getAdsPromises = []
+  const nextPageLink = $(nextPage)
+    .find(".andes-pagination__button--next > a")
+    .attr("href");
+  let ads = [];
 
   listItems.each(async (index, element) => {
-
     if (index >= total) {
       return false;
     }
-    const ad = {
-      title: "",
-      price: 0,
-      image: "",
-    };
-
-    ad.title = $(element)
-      .find(".ui-search-item__title")
-      .text();
-    ad.price = $(element)
-      .find(".ui-search-price.ui-search-price--size-medium > .ui-search-price__second-line")
-      .find(".price-tag-fraction")
-      .text();
-    ad.image = $(element).find(".slick-active > img").attr("data-src");
-
+    const ad = getAdData($, element)
     ads.push(ad);
   });
 
-  if(ads.length < total){
-    const newTotal = total - ads.length
-    let ads1 = await getAds(nextPageLink, newTotal, ads)
-    ads.push(...ads1)
-  }
+  return checkAds(ads, total, nextPageLink)
+}
 
+async function checkAds(ads, total, nextPageLink) {
+  if (ads.length < total) {
+      const newTotal = total - ads.length
+      const ads1 = await getAds(nextPageLink, newTotal, ads)
+      ads.push(...ads1)
+  }
   return ads
 }
 
-function assembleUrl(query){
-  return `https://lista.mercadolivre.com.br/${query}`
+function getAdData($, element) {
+  const ad = {
+      title: '',
+      price: 0,
+      image: ''
+  }
+
+  ad.title = $(element).find('.ui-search-item__title').text()
+  ad.price = $(element)
+      .find('.ui-search-price.ui-search-price--size-medium > .ui-search-price__second-line')
+      .find('.price-tag-fraction')
+      .text()
+  ad.image = $(element).find('.slick-active > img').attr('data-src')
+  return ad
 }
 
-async function resolveAds(){
-  const url = assembleUrl("Galaxy S21")
-  const res = await getAds(url, 45)
-  // console.log(res)
+function assembleUrl(query) {
+  return `https://lista.mercadolivre.com.br/${query}`;
 }
 
-resolveAds()
+async function resolveAds() {
+  const url = assembleUrl("Galaxy S21");
+  const res = await getAds(url, 65);
 
+  console.log(res)
+}
+
+resolveAds();
